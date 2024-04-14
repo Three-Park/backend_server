3ae3da12c3623768639fae6350dbb8a194127140
Min Gyung Park <kateking001130@ewhain.net>
2024-04-14 15:25:11 +0900

    [update]music model title length

--- fairy_tairy/recommend_music/models.py
+++ fairy_tairy/recommend_music/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 # Create your models here.
 class Music(models.Model):
-    music_title = models.CharField(max_length=50, null=True)
+    music_title = models.CharField(max_length=100, null=True)
     artist = models.CharField(max_length=100, null=True)
     genre = models.CharField(max_length=20, null=True)
     music_url = models.URLField(null=True)
