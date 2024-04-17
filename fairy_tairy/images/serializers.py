764032583a09ab855b79f99df44ab57d795ea241
Min Gyung Park <kateking001130@ewhain.net>
2024-04-17 13:34:11 +0900

    [update] clean serializers.py

--- fairy_tairy/images/serializers.py
+++ fairy_tairy/images/serializers.py
@@ -1,7 +1,6 @@
 from rest_framework import serializers
 import fairy_tairy.settings as settings
 from .models import *
-from ai.generate_image import *
 
 class ImageSerializer(serializers.ModelSerializer):
     class Meta:
