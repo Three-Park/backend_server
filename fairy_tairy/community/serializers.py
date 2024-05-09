0fc38e786d2a98641661dfaf32c49bf84b150838
Min Gyung Park <kateking001130@ewhain.net>
2024-05-09 23:10:26 +0900

    [update] community serializer

--- fairy_tairy/community/serializers.py
+++ fairy_tairy/community/serializers.py
@@ -1,8 +1,27 @@
 from rest_framework import serializers
 from diaries.models import Diary
-from users.models import Follow
+from users.models import User
+from images.serializers import ImageSerializer
+from images.models import Image
+from recommend_music.serializers import MusicSerializer
+from recommend_music.models import Music
+
+from users.models import User
 
 class CommunityDiarySerializer(serializers.ModelSerializer):
+    music = MusicSerializer(required=False)
+    image = ImageSerializer(required=False)
+    # username = UsernameSerializer(required=False)
+    
     class Meta:
         model = Diary
-        fields = ['id','user','title','content','registered_at','last_update_at', 'is_open']
+        fields = ['id','user','title','content','music','image_url','registered_at','last_update_at', 'is_open']
+
+    # def get_username(self,obj):
+    #     user = User.objects.filter(id=obj.user.id).first()
+    #     return user.username
+    
+    def get_image_url(self,obj):
+        image = Image.objects.filter(id=obj.user.id).first()
+        return image.image_url
+    
