cf9acd980c5b96379f8bdffc4145cd28dcc22d2a
Min Gyung Park <kateking001130@ewhain.net>
2024-05-11 20:58:02 +0900

    [update] follower

--- fairy_tairy/users/serializers.py
+++ fairy_tairy/users/serializers.py
@@ -6,10 +6,20 @@ from rest_framework import serializers
 
 
 class FollowSerializer(serializers.ModelSerializer):
+    follower_username = serializers.SerializerMethodField()
+    following_user_username = serializers.SerializerMethodField()
+
     class Meta:
         model = Follow
-        fields = '__all__'
+        fields = ['id', 'follower', 'following_user', 'status', 'follower_username', 'following_user_username']
 
+    def get_follower_username(self, obj):
+        return obj.follower.username if obj.follower else None
+
+    def get_following_user_username(self, obj):
+        return obj.following_user.username if obj.following_user else None
+    
+    
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
--- fairy_tairy/users/views.py
+++ fairy_tairy/users/views.py
@@ -95,8 +95,6 @@ class FollowViewSet(GenericViewSet,
             'status': Follow.REQUESTED
         }
         
-        ####
-        #serializer = self.get_serializer(data=request.data)
         serializer = self.get_serializer(data=request_data)
         serializer.is_valid(raise_exception=True)
         
